16f9b65dd488e888198555732566a94f35723fe9
NoobDev11 <67225601+NoobDev11@users.noreply.github.com>
2025-09-19 15:24:34 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // we only declare plugins with versions here, they get applied in subprojects
-    id("com.android.application") version "8.5.2" apply false
+    id("com.android.application")  apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
     id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
 }
