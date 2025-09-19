0f7f762161a714456bc9011549434167952f680c
NoobDev11 <67225601+NoobDev11@users.noreply.github.com>
2025-09-19 21:55:55 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,6 +15,16 @@ rootProject.buildDir = file("../build")
 
 subprojects {
     buildDir = file("${rootProject.buildDir}/${project.name}")
+// ✅ Force Java + Kotlin to use 17 everywhere (fixes emoji_picker_flutter mismatch)
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = JavaVersion.VERSION_17.toString()
+        targetCompatibility = JavaVersion.VERSION_17.toString()
+    }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
 }
 
 subprojects {
